178c340607070c62dd8c02e2af69d9d9970fa815
Oluwakemi Audu <audukemi@gmail.com>
2022-09-15 07:20:32 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 /**
- *uses the put command to print.
+ *main=entry point
+ *
+ *puts command prints.
+ *
  */
 int main(void)
 {
-  puts ("Programming is like building a multilingual puzzle\n");
+  puts ("\"Programming is like building a multilingual puzzle\n");
   return (0);
 }

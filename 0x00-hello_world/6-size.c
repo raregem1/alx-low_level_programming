264bc61e6fd5f9e9c59aa2049d316e094a3672d9
Oluwakemi Audu <audukemi@gmail.com>
2022-09-17 13:38:53 -0700

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,3 +1,21 @@
 #include<stdio.h>
 /**
- *main- 
+ *main- Prints the size of various tyes using printf cmd
+ *
+ *Return: 0
+ */
+int main(void)
+{ int a;
+  long int b;
+  long long int c;
+  char d;
+  float e;
+
+printf("Size of a char: %mu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of an int: %mu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a long int: %mu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %mu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a float: %mu byte(s)\n", (unsigned long)sizeof(e));
+
+return	(0);
+}

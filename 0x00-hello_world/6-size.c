1697f47ed85f247d75f28fb4cb571a290d24b6cc
Oluwakemi Audu <audukemi@gmail.com>
2022-09-17 13:47:02 -0700

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,17 @@
  *Return: 0
  */
 int main(void)
-{ int a;
-  long int b;
-  long long int c;
-  char d;
-  float e;
+{int a;
+long int b;
+long long int c;
+char d;
+float e;
 
-printf("Size of a char: %mu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of an int: %mu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a long int: %mu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long long int: %mu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a float: %mu byte(s)\n", (unsigned long)sizeof(e));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 
 return	(0);
 }
